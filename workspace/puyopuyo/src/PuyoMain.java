8b28d789cae0ecf63a1a736d6c6cce38f28df567
Rizwan Chaudry <chaudryrizwan@gmail.com>
2017-06-20 16:29:48 -0400

    Added JFrame

---
+++ workspace/puyopuyo/src/PuyoMain.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class PuyoMain {
+	public static void main(String[] args) {
+		JOptionPane window = new JOptionPane();
+		JFrame frame = new JFrame();
+		frame.setSize(200, 400);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setFocusable(true);
+	}
+}

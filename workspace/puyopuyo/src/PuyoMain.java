c8d2a6bc1ccd454155a8919ccc3585544bc616d3
Rizwan Chaudry <chaudryrizwan@gmail.com>
2017-07-07 15:48:46 -0400

    Added default close operation

--- workspace/puyopuyo/src/PuyoMain.java
+++ workspace/puyopuyo/src/PuyoMain.java
@@ -5,9 +5,11 @@ public class PuyoMain {
 	public static void main(String[] args) {
 		JOptionPane window = new JOptionPane();
 		JFrame frame = new JFrame();
+		frame.add(new PuyoBoard());
 		frame.setSize(200, 400);
 		frame.setVisible(true);
-		frame.setResizable(false);
+		//frame.setResizable(false);
 		frame.setFocusable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
